e3652619e3288f4fb27258415854dc07ad955d0f
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-03 08:04:36 -0300

    feat: create theme file

---
+++ src/theme/index.ts
@@ -0,0 +1,38 @@
+export default {
+  COLORS: {
+    RED: {
+      DARK: '#BF3B44',
+      MID: '#F3BABD',
+      LIGHT: '#F4E6E7',
+    },
+    GREEN: {
+      DARK: '#639339',
+      MID: '#CBE4B4',
+      LIGHT: '#E5F0DB'
+    },
+    GRAY: {
+      '100': '#1B1D1E',
+      '200': '#333638',
+      '300': '#5C6265',
+      '400': '#B9BBBC',
+      '500': '#DDDEDF',
+      '600': '#EFF0F0',
+      '700': '#FAFAFA',
+    },
+    WHITE: '#FFFFFF',
+  },
+  FONT_FAMILY: {
+    NUNITO_SANS: {
+      REGULAR: 'NunitoSans_400Regular',
+      BOLD: 'NunitoSans_700Bold'
+    }
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    '2XL': 32,
+  }
+}
