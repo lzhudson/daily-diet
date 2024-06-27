365cb3482e9904e3143bc8eb1fff852cd3162698
Hudson Holanda <hudsonholanda55@gmail.com>
2024-06-26 21:32:26 -0300

    feat: envolve application in SafeAreaProvider

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { StatusBar } from 'react-native';
+import { Home } from '@screens/Home';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
-        barStyle="light-content"
+        barStyle="dark-content"
         backgroundColor="transparent"
-        translucent
       />
+      <SafeAreaProvider>
+        <Home />
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 }
